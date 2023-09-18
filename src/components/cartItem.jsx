3f048808b079985b4a24305809e9684bccbaf2b1
Aslamsidheeq <aslamsidhik22@gmail.com>
2023-09-18 12:04:52 +0530

    products quantity working added

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,21 +1,32 @@
 import {ChevronDown,ChevronUp} from '../icon'
+import { useDispatch } from 'react-redux'
+import {removeItem,increase, decrease} from '../features/cart/cartSlice'
 
+//lives in cartContainer component
 const CartItem = ({id,img,title,price,amount}) =>{
+    const dispatch = useDispatch(); //returns from redux store
     return(
         <article className='cart-item'>
             <img src={img} alt={title}/>
             <div>
                 <h4>{title}</h4>
                 <h4 className="item-price">${price}</h4>
-                <button className="remove-btn">remove</button>
+                {/* type:cart/removeItem and payload: defxxx989 id ; ItemIdFromDispatch @ cartSlice */}
+                <button className="remove-btn" onClick={()=>{dispatch(removeItem(id))}}>remove</button>        
             </div>
             <div>
-                <button className="amount-btn" >
-                    <ChevronUp/>
+                <button className="amount-btn" onClick={()=>dispatch(increase({id}))}>
+                    <ChevronUp />
                 </button>
                 <p className='amount'>{amount}</p>
-                <button className="amount-btn">
-                    <ChevronDown/>
+                <button className="amount-btn" onClick={()=>{
+                    if(amount===1){
+                    dispatch(removeItem(id));
+                    return;
+                    }
+                    dispatch(decrease({id}))
+                    }}>
+                    <ChevronDown />
                 </button>
             </div>
         </article>
--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,5 +1,6 @@
 import {createSlice} from '@reduxjs/toolkit';
 import cartItems from '../../components/cartItems';
+
 const initialState = {
     cartItems: cartItems,
     amount:4,
@@ -13,10 +14,24 @@ const cartSlice = createSlice({
     reducers:{
         clearCart: (state) =>{
             state.cartItems=[];
-        }
+        },
+        removeItem:(state,{payload})=>{
+            state.cartItems = state.cartItems.filter((item)=>
+            item.id !== payload)
+        },
+        increase:(state,{payload})=>{
+            const cartItem =  state.cartItems.find((item)=>
+            item.id === payload.id) 
+            cartItem.amount = cartItem.amount+1;
+        },
+        decrease:(state,{payload})=>{
+            const itemm=  state.cartItems.find((item)=>
+            item.id === payload.id)
+            itemm.amount = itemm.amount-1;
+        },
     }
 })
 // console.log(cartSlice)
 
-export const {clearCart} = cartSlice.actions;
+export const {clearCart,removeItem,increase,decrease} = cartSlice.actions;
 export default cartSlice.reducer;
